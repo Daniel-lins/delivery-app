117f36e24d2b61e28282b00ba06d2bfb8d6a78e1
Daniel Lins <dlins300@gmail.com>
2022-07-20 17:24:12 -0300

    feat: product-card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from "react";
+
 import Helmet from "../Componentes/Helmet/Helmet";
 import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
 
@@ -10,7 +12,15 @@ import featureImg01 from "../assets/images/service-01.png";
 import featureImg02 from "../assets/images/service-02.png";
 import featureImg03 from "../assets/images/service-03.png";
 
+import products from "../assets/fake-data/products.js";
+
+import foodCategoryImg01 from "../assets/images/hamburger.png";
+import foodCategoryImg02 from "../assets/images/pizza.png";
+import foodCategoryImg03 from "../assets/images/bread.png";
+
 import { Link } from "react-router-dom";
+
+import ProductCard from "../Componentes/UI/product-card/ProductCard";
 
 import Category from "../Componentes/UI/category/Category";
 
@@ -33,6 +43,8 @@ const featureData = [
   },
 ];
 export const Home = () => {
+  const [allProducts, setAllProducts] = useState(products);
+  const [category, setCategory] = useState("ALL");
   return (
     <Helmet title="Home">
       <section>
@@ -122,6 +134,64 @@ export const Home = () => {
                   <h5 className=" fw-bold mb-3">{item.title}</h5>
                   <p>{item.desc}</p>
                 </div>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center">
+              <h2>Popular Foods</h2>
+            </Col>
+
+            <Col lg="12">
+              <div className="food__category d-flex align-items-center justify-content-center gap-4">
+                <button
+                  className={`all__btn  ${
+                    category === "ALL" ? "foodBtnActive" : ""
+                  } `}
+                  onClick={() => setCategory("ALL")}
+                >
+                  All
+                </button>
+                <button
+                  className={`d-flex align-items-center gap-2 ${
+                    category === "BURGER" ? "foodBtnActive" : ""
+                  } `}
+                  onClick={() => setCategory("BURGER")}
+                >
+                  <img src={foodCategoryImg01} alt="" />
+                  Burger
+                </button>
+
+                <button
+                  className={`d-flex align-items-center gap-2 ${
+                    category === "PIZZA" ? "foodBtnActive" : ""
+                  } `}
+                  onClick={() => setCategory("PIZZA")}
+                >
+                  <img src={foodCategoryImg02} alt="" />
+                  Pizza
+                </button>
+
+                <button
+                  className={`d-flex align-items-center gap-2 ${
+                    category === "BREAD" ? "foodBtnActive" : ""
+                  } `}
+                  onClick={() => setCategory("BREAD")}
+                >
+                  <img src={foodCategoryImg03} alt="" />
+                  Bread
+                </button>
+              </div>
+            </Col>
+
+            {allProducts.map((item) => (
+              <Col lg="3" md="4" sm="6" xs="6" key={item.id} className="mt-5">
+                <ProductCard item={item} />
               </Col>
             ))}
           </Row>
