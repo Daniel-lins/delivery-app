4400e7652be8e7d2a8aad9e02db0edd24b24a83d
Daniel Lins <dlins300@gmail.com>
2022-07-21 16:24:24 -0300

    feat: page All-Food, searchTerm, Pagination

--- src/pages/AllFoods.jsx
+++ src/pages/AllFoods.jsx
@@ -1,5 +1,97 @@
+import React, { useState } from "react";
+
+import { Container, Row, Col } from "reactstrap";
+
+import products from "../assets/fake-data/products";
+
+import ProductCard from "../Componentes/UI/product-card/ProductCard";
+import ReactPaginate from "react-paginate";
+
+import Helmet from "../Componentes/Helmet/Helmet";
+
+import "../styles/all-foods.css";
+import "../styles/pagination.css";
+
 export const AllFoods = () => {
-  return <div>AllFoods</div>;
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const searchedProduct = products.filter((item) => {
+    if (searchTerm.value === "") {
+      return item;
+    }
+    if (item.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+      return item;
+    } else {
+      return console.log("not found");
+    }
+  });
+
+  const productPerPage = 12;
+  const visitedPage = pageNumber * productPerPage;
+  const displayPage = searchedProduct.slice(
+    visitedPage,
+    visitedPage + productPerPage
+  );
+
+  const pageCount = Math.ceil(searchedProduct.length / productPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
+  return (
+    <Helmet title="All-Foods">
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="6" xs="12">
+              <div className="search__widget d-flex align-items-center justify-content-between ">
+                <input
+                  type="text"
+                  placeholder="Estou procurando por ..."
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <span>
+                  <i class="ri-search-line"></i>
+                </span>
+              </div>
+            </Col>
+
+            <Col lg="6" md="6" sm="6" xs="12" className="mb-5">
+              <div className="sorting__widget text-end">
+                <select className="w-50">
+                  <option>Padrão</option>
+                  <option value="ascending">Ordem Alfabética, A-Z</option>
+                  <option value="descending">Ordem Alfabética, Z-A</option>
+                  <option value="high-price">Maior Preço</option>
+                  <option value="low-price">Menor Preço</option>
+                </select>
+              </div>
+            </Col>
+
+            {displayPage.map((item) => (
+              <Col lg="3" md="4" sm="6" xs="6" key={item.id} className="mb-4">
+                <ProductCard item={item} />
+              </Col>
+            ))}
+
+            <div>
+              <ReactPaginate
+                pageCount={pageCount}
+                onPageChange={changePage}
+                previousLabel={"Prev"}
+                nextLabel={"Next"}
+                containerClassName=" paginationBttns "
+              />
+            </div>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default AllFoods;
