90753e25e3b7d28e30d69249563021c6046d384e
Daniel Lins <dlins300@gmail.com>
2022-07-21 23:45:25 -0300

    feat: Page/Register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,61 @@
-export const Register = () => {
-  return <div>Register</div>;
+import React, { useRef } from "react";
+import Helmet from "../Components/Helmet/Helmet";
+import CommonSection from "../Components/UI/common-section/CommonSection";
+import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
+
+const Register = () => {
+  const signupNameRef = useRef();
+  const signupPasswordRef = useRef();
+  const signupEmailRef = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Helmet title=" Cadastro">
+      <CommonSection title="Cadastro" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="12" className="m-auto text-center">
+              <form className="form mb-5" onSubmit={submitHandler}>
+                <div className="form__group">
+                  <input
+                    type="text"
+                    placeholder="Nome Completo"
+                    required
+                    ref={signupNameRef}
+                  />
+                </div>
+                <div className="form__group">
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    required
+                    ref={signupEmailRef}
+                  />
+                </div>
+                <div className="form__group">
+                  <input
+                    type="password"
+                    placeholder="Senha"
+                    required
+                    ref={signupPasswordRef}
+                  />
+                </div>
+                <button type="submit" className="addTOCart__btn">
+                  Cadastrar
+                </button>
+              </form>
+              <Link to="/login">Já tem uma Conta? Login</Link>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
+
 export default Register;
