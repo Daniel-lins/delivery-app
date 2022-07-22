abaf89a07297376916c417f2ec9f431875752701
Daniel Lins <dlins300@gmail.com>
2022-07-22 00:00:28 -0300

    feat: Page/checkout

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,5 +1,124 @@
-export const Checkout = () => {
-  return <div>Checkout</div>;
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { Container, Row, Col } from "reactstrap";
+import CommonSection from "../Components/UI/common-section/CommonSection";
+import Helmet from "../Components/Helmet/Helmet";
+
+import "../styles/checkout.css";
+
+const Checkout = () => {
+  const [enterName, setEnterName] = useState("");
+  const [enterEmail, setEnterEmail] = useState("");
+  const [enterNumber, setEnterNumber] = useState("");
+  const [enterCountry, setEnterCountry] = useState("");
+  const [enterCity, setEnterCity] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+
+  const shippingInfo = [];
+  const cartTotalAmount = useSelector((state) => state.cart.totalAmount);
+  const shippingCost = 30;
+
+  const totalAmount = cartTotalAmount + Number(shippingCost);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const userShippingAddress = {
+      name: enterName,
+      email: enterEmail,
+      phone: enterNumber,
+      country: enterCountry,
+      city: enterCity,
+      postalCode: postalCode,
+    };
+
+    shippingInfo.push(userShippingAddress);
+    console.log(shippingInfo);
+  };
+
+  return (
+    <Helmet title=" Finalizar">
+      <CommonSection title="Finalizar" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="8" md="6">
+              <h6 className="mb-4">endereço de entrega</h6>
+              <form className="checkout__form" onSubmit={submitHandler}>
+                <div className="form__group">
+                  <input
+                    type="text"
+                    placeholder="Digite seu nome"
+                    required
+                    onChange={(e) => setEnterName(e.target.value)}
+                  />
+                </div>
+
+                <div className="form__group">
+                  <input
+                    type="email"
+                    placeholder="Digite seu Email"
+                    required
+                    onChange={(e) => setEnterEmail(e.target.value)}
+                  />
+                </div>
+                <div className="form__group">
+                  <input
+                    type="number"
+                    placeholder="Numero de telefone"
+                    required
+                    onChange={(e) => setEnterNumber(e.target.value)}
+                  />
+                </div>
+                <div className="form__group">
+                  <input
+                    type="text"
+                    placeholder="País"
+                    required
+                    onChange={(e) => setEnterCountry(e.target.value)}
+                  />
+                </div>
+                <div className="form__group">
+                  <input
+                    type="text"
+                    placeholder="Cidade"
+                    required
+                    onChange={(e) => setEnterCity(e.target.value)}
+                  />
+                </div>
+                <div className="form__group">
+                  <input
+                    type="number"
+                    placeholder="CEP"
+                    required
+                    onChange={(e) => setPostalCode(e.target.value)}
+                  />
+                </div>
+                <button type="submit" className="addTOCart__btn">
+                  Pagar
+                </button>
+              </form>
+            </Col>
+
+            <Col lg="4" md="6">
+              <div className="checkout__bill">
+                <h6 className="d-flex align-items-center justify-content-between mb-3">
+                  Subtotal: <span>${cartTotalAmount}</span>
+                </h6>
+                <h6 className="d-flex align-items-center justify-content-between mb-3">
+                  Entrega: <span>R${shippingCost}</span>
+                </h6>
+                <div className="checkout__total">
+                  <h5 className="d-flex align-items-center justify-content-between">
+                    Total: <span>R${totalAmount}</span>
+                  </h5>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Checkout;
